5400f13f1e85fd8eb160e80348b56acc80081b2e
Selwyn <talk@selwyn.cc>
2016-11-22 13:05:36 +0100

    fix(build): fix ES module exporting the default property

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// The ES module proposal only supports named imports and exports
+// All this does is assign 'default' to what gets exported
+
+import PriceCsvParser from './main'
+
+module.exports = PriceCsvParser
