56220b8e90613a9bdbd2684bc91ff4dd1e3cbfbd
Selwyn <talk@selwyn.cc>
2016-11-28 11:18:36 +0100

    fix(user-agent): search for the SDK package.json from the root
    Instead of getting the SDK version from the package's JSON file itself search for it from the root to fix the usage of this module as a dependency.
    
    Resolves #9

--- src/user-agent.js
+++ src/user-agent.js
@@ -1,9 +1,9 @@
 import { arch, platform } from 'os'
+import getPackage from 'load-module-pkg'
 
 import modulePackage from '../package.json'
-import sdkPackage from '../node_modules/sphere-node-sdk/package.json'
 
-const sdkInfo = `node-sdk/${sdkPackage.version}`
+const sdkInfo = `node-sdk/${getPackage('sphere-node-sdk').version}`
 const nodeInfo = `Node.js/${process.version}`
 const runtimeInfo = `${platform()}; ${arch()}`
 const moduleInfo = `${modulePackage.name}/${modulePackage.version}`
